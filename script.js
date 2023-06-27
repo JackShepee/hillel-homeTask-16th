794d2f99c33868ea370dba73e8bc0493cacd25c6
JackShepee <salamakhaviktor@gmail.com>
2023-06-27 13:49:46 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const images = ["1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png"];
+let currentIndex = 0;
+
+const imageElement = document.getElementById("image");
+const prevButton = document.getElementById("prev-btn");
+const nextButton = document.getElementById("next-btn");
+
+function updateImage() {
+  imageElement.src = "images/" + images[currentIndex];
+  prevButton.hidden = currentIndex === 0;
+  nextButton.hidden = currentIndex === images.length - 1;
+}
+
+prevButton.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateImage();
+  }
+});
+
+nextButton.addEventListener("click", () => {
+  if (currentIndex < images.length - 1) {
+    currentIndex++;
+    updateImage();
+  }
+});
+
+updateImage();
